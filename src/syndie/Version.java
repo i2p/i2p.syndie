24167c1ba394a0ecab792f0440fbe87ba3895887
kytv <kytv@mail.i2p>
2013-01-07 22:17:48 +0000

    fix javadocs target, add simple jdoc

--- src/syndie/Version.java
+++ src/syndie/Version.java
@@ -1,5 +1,11 @@
 package syndie;
 
+/**
+ * Exposes a version string
+ */
 public class Version {
+   /**
+    * for example "1.102b-1"
+    */
     public static final String VERSION = "1.102b-2";
 }
